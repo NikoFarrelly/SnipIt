91134a64e66d9effa211e4ff48df4681cadfdefe
NikoFarrelly <nickhfarrelly@gmail.com>
2026-01-29 11:08:10 +0000

    added URL prefill

--- src/popup.ts
+++ src/popup.ts
@@ -6,15 +6,34 @@ interface SubmitResponseProps {
 
 type SubmitResponse = null | SubmitResponseProps;
 
+let activeTab: browser.tabs.Tab;
+
+const main = async () => {
+
+    const tabs = await browser.tabs.query({active: true, currentWindow: true});
+    if (!tabs) return;
+    const activeTab = tabs[0];
+    if (!activeTab || !activeTab.url) return;
+
+    prefillURL(activeTab.url);
+}
+
+main();
+
+const prefillURL = (url: string) => {
+    const urlInput = document.getElementById('url') as HTMLInputElement | null;
+    if (urlInput) {
+        urlInput.value = url;
+    }
+}
+
 document.getElementById("snipBtn")?.addEventListener("click", async () => {
 
     const fromText = (document.getElementById("fromText") as HTMLInputElement).value;
     const untilClassName = (document.getElementById("untilClass") as HTMLInputElement).value;
 
-    const tabs = await browser.tabs.query({active: true, currentWindow: true});
-    if (!tabs) return;
-    const activeTab = tabs[0];
-    if (!activeTab || !activeTab.id) return;
+    if (!activeTab?.id) return;
+
 
     const submit: SubmitResponse = await browser.tabs.sendMessage(activeTab.id, {
         fromText,
