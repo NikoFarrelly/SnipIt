2ca1a4eb6fc4228aa0c1c9fcba8991fe7ea57dc8
NikoFarrelly <nickhfarrelly@gmail.com>
2026-02-09 16:34:37 +0000

    added hotkey open support

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,14 @@ import {defineConfig} from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     manifest: {
+        commands: {
+            _execute_browser_action: {
+                description: 'Opens the SnipIt extension.',
+                suggested_key: {
+                    default: 'Command+Alt+S',
+                }
+            },
+        },
         permissions: ['storage', 'tabs', 'scripting'],
     },
 });
